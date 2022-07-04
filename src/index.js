ddc02fd1554a02a0f8693bda8bcd691a4f0419e9
xJoeCode <xjosephtbr@gmail.com>
2022-07-04 18:51:51 +0800

    added constuctor function for adding task taking values from the form and pushing it to array

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import './styles.css'
+import "./styles.css";
 
+document.querySelector("#closeButton").onclick = function () {
+    closeForm();
+};
+document.querySelector("#taskSubmit").onclick = function () {
+    const title = document.getElementById("title").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+    const description = document.getElementById("description").value;
+    const priority = document.getElementById("priority").value;
+    if (title == "") {
+        alert("Title fields are empty");
+    } else if (date == "") {
+        alert("Date fields are empty");
+    } else if (time == "") {
+        alert("Time fields are empty");
+    } else {
+        pushToArray(title, date, time, description, priority);
+        closeForm()
+    }
+};
 
-document.querySelector("#closeButton").onclick = function() {closeForm()}
-
-function closeForm()  {
-    console.log("closing form")
-   const form = document.querySelector("#formContainer")
-   form.classList.add("hidden")
-
-  
+function closeForm() {
+    console.log("closing form");
+    const form = document.querySelector("#formContainer");
+    form.classList.add("hidden");
 }
 
+let myTasks = [];
+
+class Task {
+    constructor(title, date, time, description, priority) {
+        this.title = title;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.priority = priority;
+    }
+}
+
+const pushToArray = (title, date, time, description, priority) => {
+    let task = new Task(title, date, time, description, priority);
+    myTasks.push(task);
+    console.table(myTasks);
+};
